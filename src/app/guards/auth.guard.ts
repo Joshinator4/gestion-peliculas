d499cfd32352008e6ab6309b3114e80e840fb7a8
DAM2 <wowmewet1@gmail.com>
2025-03-11 16:32:34 +0100

    Subiendo cambio en docs

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,22 @@
-import { inject, Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import { Router, CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, Route, UrlSegment, CanMatchFn } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from 'src/services/auth.service';
 
+import { AuthService } from 'src/services/auth.service';
+import { UsersService } from '../users/services/users.service';
 
 const checkAuthStatus = (): boolean => {
-  const authService: AuthService = inject(AuthService)
+  const usersService: UsersService = inject(UsersService)
   const router: Router = inject(Router)
 
+  usersService.onExisteUsuario(localStorage.getItem('token') ? localStorage.getItem('token')! : "");
+  let existeUser = usersService.existeUser;
   if(!localStorage.getItem('token')){
     router.navigate(['/'])
   }
   return true;
 }
+
+
 
 export const canActivateGuard: CanActivateFn = ( //tipado CanActivate
   route: ActivatedRouteSnapshot,
